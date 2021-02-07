d15917bee76df0b0636d5477843a3c47fc5d3004
Lukas <stratocaster@live.dk>
2021-02-07 17:20:38 +0100

    Adding practice files from other project

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <h1>Dette er HTML</h1>
-    </body>
-</html>
+<html lang="da">
+  <head>
+    <meta charset="utf-8" />
+    <title>Webserver</title>
+    <link href="css/styles.css" rel="stylesheet" type="text/css" />
 
+    <meta name="viewport" content="width=device-wiidth, initial-scale=1" />
+  </head>
+  <body>
+    <?php include "include/navigation.php"; ?>
+    <div class="main">
+      <div>Lorem ipsum</div>
+    </div>
+    <?php include "include/footer.php"; ?>
+  </body>
+</html>
